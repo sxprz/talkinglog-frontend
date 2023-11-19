95ff674976bd31d14d5676d43d6db4449adda213
suprize <78696407+sxprz@users.noreply.github.com>
2023-11-19 09:48:52 +0100

    Fix Bar component (#4)
    * Impl chat bar (WIP), use first API endpoint
    
    * Fix Bar component
    
    * Impl /prompt and /check endpoints client-side
    
    * Prepare app for error handling on new endpoints
    
    * Impl /demo endpoint and prepare err handling
    
    * Impl /history endpoint
    
    * Fix last bugs and impl ToastManager for future use
    
    * Impl spinning wheel for progress indication
    
    * Fix wrong types for several signals
    
    * Improve visual representation of chat msg for hackaTUM presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Signal, signal } from '@preact/signals-react'
 import './App.css'
 import { Content } from './views/Content';
-import { Stack } from '@mui/system';
+import { Box, Stack } from '@mui/system';
 import Bar from './components/Bar';
-import { AppBar, Grid, Toolbar } from '@mui/material';
+import { AppBar, Button, CircularProgress, Grid, Toolbar, Typography } from '@mui/material';
 import Chatbar from './components/Chatbar';
+import { Build } from '@mui/icons-material';
+import axios from 'axios';
 
-export enum RawUploadStatus {
+export enum Status {
   IDLE,
-  UPLOADING,
+  RUNNING,
   SUCCESS,
   FAILURE
 }
@@ -18,8 +20,8 @@ export enum ConversationSide {
   USER
 }
 
-export type UploadStatus = {
-  status: RawUploadStatus,
+export type ProgressStatus = {
+  status: Status,
   additionalInfo: string
 }
 
@@ -29,24 +31,66 @@ export type ConversationPart = {
 }
 
 export const sidebarSignal = signal(false);
-export const uploadedFilesSignal = signal({ status: RawUploadStatus.IDLE, additionalInfo: "" });
+export const areFilesUploadedSignal = signal<ProgressStatus>({ status: Status.IDLE, additionalInfo: "" });
 export const currentSessionIdSignal = signal("");
-export const conversationSignal = signal([]) as unknown as Signal<[ConversationPart]>;
+export const conversationSignal = signal<ConversationPart[]>([]);
+export const promptTextSignal = signal("");
+export const promptProcessingSignal = signal<ProgressStatus>({ status: Status.IDLE, additionalInfo: "" });
+export const chatHistoryStoreSignal = signal<string[]>([]);
+export const historyProcessingSignal = signal<ProgressStatus>({ status: Status.IDLE, additionalInfo: "" });
 export const darkModeSignal = signal(false);
 
 function App() {
+
+  const baseUrl = `https://immortal-up-weevil.ngrok-free.app`;
+  const demoUrl = baseUrl + `/demo`; 
+
+  const onTriggerDemo = () => {
+    areFilesUploadedSignal.value = { status: Status.RUNNING, additionalInfo: "" };
+
+    axios({
+        url: demoUrl,
+        withCredentials: false, 
+        method: `POST`,
+    }).then(res => {
+        const data = res.data;
+        if (data !== null) {
+            areFilesUploadedSignal.value = { status: Status.SUCCESS, additionalInfo: "" };
+        } else {
+            areFilesUploadedSignal.value = { status: Status.FAILURE, additionalInfo: "Demo failed: No further information" };
+        }
+        currentSessionIdSignal.value = data;
+    }, err => {
+        areFilesUploadedSignal.value = { status: Status.FAILURE, additionalInfo: err };
+    });
+
+  };
+
   return (
-    <Stack spacing={5} width="100vw">
-      <Bar />
-      <Content />
-      <AppBar position="sticky" sx={{ backgroundColor: "transparent" }}>
-        <Toolbar>
-        <Grid container justifyContent="center">
-          <Chatbar />
-        </Grid>
-        </Toolbar>
-      </AppBar>
-    </Stack>
+    <div>
+      <Stack spacing={5} width="100vw" sx={{ alignContent: "center" }}>
+        <Bar />
+        <Content />
+        <AppBar position="sticky" elevation={0} sx={{ backgroundColor: "transparent" }}>
+          <Toolbar>
+          <Grid container justifyContent="center">
+            <Button component="label" variant="outlined" onClick={onTriggerDemo}>
+              <Build />
+              <Typography sx={{ textTransform: "none" }}>
+                  Use Demo Data
+              </Typography>
+            </Button>
+            <Chatbar />
+            { areFilesUploadedSignal.value.status === Status.RUNNING || promptProcessingSignal.value.status === Status.RUNNING ?
+            <Box sx={{ display: 'flex' }}>
+              <CircularProgress />
+            </Box> :
+            null }
+          </Grid>
+          </Toolbar>
+        </AppBar>
+      </Stack>
+    </div>
   )
 }
 
--- src/components/Chatbar.tsx
+++ src/components/Chatbar.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Input as BaseInput } from '@mui/base/Input';
 import { styled } from '@mui/system';
-import { Send } from '@mui/icons-material';
+import { ConversationSide, Status, conversationSignal, currentSessionIdSignal, promptTextSignal, areFilesUploadedSignal, promptProcessingSignal } from '../App';
+import axios from 'axios';
 
 const Input = React.forwardRef(function CustomInput(
   props: React.InputHTMLAttributes<HTMLInputElement>,
@@ -10,8 +11,61 @@ const Input = React.forwardRef(function CustomInput(
   return <BaseInput slots={{ input: InputElement }} {...props} ref={ref} />;
 });
 
+const baseUrl = `https://immortal-up-weevil.ngrok-free.app`;
+const promptUrl = baseUrl + `/prompt`;
+const checkUrl = baseUrl + `/check`;
+
+const onSend = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+        promptProcessingSignal.value = { status: Status.RUNNING, additionalInfo: "" };
+
+        // Listen to server for results every 5 seconds as it reduces the number of available connections from the server after sending the input
+        const promptFormdata = new FormData();
+        const checkFormdata = new FormData();
+        const sessionId = currentSessionIdSignal.value;
+
+        promptFormdata.append("session", sessionId);
+        promptFormdata.append("prompt", promptTextSignal.value);
+        checkFormdata.append("session", sessionId);
+
+        axios({
+            url: promptUrl,
+            method: `POST`,
+            data: promptFormdata
+        }).then(() => {
+            
+            const interval = setInterval(() => {
+
+                axios({
+                    url: checkUrl,
+                    method: `POST`,
+                    data: checkFormdata
+                }).then(res => {
+                    const response : string = res.data;
+                    if (response !== "waiting") {
+                        promptProcessingSignal.value = { status: Status.SUCCESS, additionalInfo: "" };
+                        conversationSignal.value.unshift({ side: ConversationSide.AI, message: response });
+                        console.log(conversationSignal.value)
+                        clearInterval(interval);
+                    }
+                }, err => {
+                    promptProcessingSignal.value = { status: Status.FAILURE, additionalInfo: err };
+                    clearInterval(interval);
+                });
+    
+            }, 5000);
+
+        }, err => {
+            promptProcessingSignal.value = { status: Status.FAILURE, additionalInfo: err };
+        });
+        
+        event.preventDefault();
+    }
+ }
+ 
+
 export default function Chatbar() {
-  return <Input style={{ alignSelf: "center" }} aria-label="Demo input" placeholder="Upload file(s) to start talking with your log files..."><Send/></Input>;
+  return <Input onKeyDownCapture={onSend} disabled={areFilesUploadedSignal.value.status !== Status.SUCCESS} style={{ alignSelf: "center" }} aria-label="Demo input" placeholder="Upload file(s) to start talking with your log files..." />;
 }
 
 const blue = {
