fb8512fd1cf9d8227cb9d1af7b6e2894bc276cac
sxprz <derian.pljakic@gmail.com>
2023-11-19 02:37:37 +0100

    Impl chat bar (WIP), use first API endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
-import { signal } from '@preact/signals-react'
+import { Signal, signal } from '@preact/signals-react'
 import './App.css'
-import Bar from './components/Bar'
-import { Content } from './components/Content';
+import { Content } from './views/Content';
+import { Stack } from '@mui/system';
+import Bar from './components/Bar';
+import { AppBar, Grid, Toolbar } from '@mui/material';
+import Chatbar from './components/Chatbar';
+
+export enum RawUploadStatus {
+  IDLE,
+  UPLOADING,
+  SUCCESS,
+  FAILURE
+}
+
+export enum ConversationSide {
+  AI,
+  USER
+}
+
+export type UploadStatus = {
+  status: RawUploadStatus,
+  additionalInfo: string
+}
+
+export type ConversationPart = {
+  side: ConversationSide,
+  message: string
+}
 
 export const sidebarSignal = signal(false);
+export const uploadedFilesSignal = signal({ status: RawUploadStatus.IDLE, additionalInfo: "" });
+export const currentSessionIdSignal = signal("");
+export const conversationSignal = signal([]) as unknown as Signal<[ConversationPart]>;
+export const darkModeSignal = signal(false);
 
 function App() {
   return (
-    <div >
-      <Bar/>
-      <Content/>
-    </div>
+    <Stack spacing={5} width="100vw">
+      <Bar />
+      <Content />
+      <AppBar position="sticky" sx={{ backgroundColor: "transparent" }}>
+        <Toolbar>
+        <Grid container justifyContent="center">
+          <Chatbar />
+        </Grid>
+        </Toolbar>
+      </AppBar>
+    </Stack>
   )
 }
 
--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,10 +1,11 @@
 import rusLogo from '../assets/rus-logo.svg';
-import { sidebarSignal } from '../App';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
+import { ConversationSide, RawUploadStatus, conversationSignal, currentSessionIdSignal, darkModeSignal, sidebarSignal, uploadedFilesSignal } from '../App';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Sidebar } from './Sidebar';
-import { IconButton, Typography } from '@mui/material';
+import { Button, IconButton, Typography, AppBar, Toolbar } from '@mui/material';
+import { Stack } from '@mui/system';
+import { Brightness2, FileUpload, WbSunnyOutlined } from '@mui/icons-material';
+import axios from "axios";
 
 export default function Bar() {
     
@@ -12,20 +13,101 @@ export default function Bar() {
         sidebarSignal.value = true;
     };
 
+    const onUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.currentTarget.files;
+        console.log(files);
+        const filesFormdata = new FormData();
+        Array.from(files ?? []).forEach(file => filesFormdata.append("logfile", file));
+        
+        uploadedFilesSignal.value = { status: RawUploadStatus.UPLOADING, additionalInfo: "" };
+        console.log("Uploading...");
+
+        axios({
+            url: `https://2150-2a09-80c0-192-0-815a-fe17-49cd-206c.ngrok-free.app//upload`,
+            method: `POST`,
+            data: filesFormdata
+        }).then(res => {
+
+            currentSessionIdSignal.value = res.data;
+            // Continue to listen to server every 5 seconds as it reduces the number of available connections from the server
+            console.log("Uploading... (from inside the async method)");
+            uploadedFilesSignal.value = { status: RawUploadStatus.UPLOADING, additionalInfo: "" };
+            
+            const checkFormdata = new FormData();
+            checkFormdata.append("session", currentSessionIdSignal.value);
+
+            setInterval(() => {
+
+                console.log('Listening to server changes');
+                axios({
+                    url: `https://2150-2a09-80c0-192-0-815a-fe17-49cd-206c.ngrok-free.app//upload`,
+                    method: `POST`,
+                    data: checkFormdata
+                }).then(res => {
+                    const prompts : string = res.data;
+                    if (prompts !== "waiting") {
+                        uploadedFilesSignal.value = { status: RawUploadStatus.SUCCESS, additionalInfo: "" };
+                        conversationSignal.value.unshift({ side: ConversationSide.AI, message: prompts });
+                        console.log("Upload success, added prompt to current session memory!");
+                    }
+
+                }, err => {
+                    uploadedFilesSignal.value = { status: RawUploadStatus.FAILURE, additionalInfo: err };
+                    console.log("Upload failed!");
+                });
+
+            }, 5000);
+
+        }, err => {
+            uploadedFilesSignal.value = { status: RawUploadStatus.FAILURE, additionalInfo: err };
+            console.log("Upload failed!");
+        });
+    };
+    
     return (
-        <AppBar sx={{ top: 0, bottom: "auto", backgroundColor: "#0F194C" }}>
+        <AppBar sx={{ backgroundColor: "#0F194C" , overflow: 'hidden'}}>
             <Toolbar>
                 <IconButton aria-label="menu" sx={{ p: '10px' }} onClick={onClick}>
                     <MenuIcon htmlColor="#FFFFFF"/>
                 </IconButton>
                 <img src={rusLogo} className="logo rus align-left" alt="RuS logo" style={{ height: "68px", width: "auto", padding: "5pt 1.75em" }}/>
-                <div style={{ display: "flex" }}>
-                <Typography noWrap>
-                    Conversation
-                </Typography>
-                <Typography noWrap>
-                    Analysis
-                </Typography>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    {uploadedFilesSignal.value.status == RawUploadStatus.SUCCESS ?
+                        (<Stack
+                            direction="row"
+                            spacing={2}
+                            sx={{ display: 'flex', justifyContent: 'center', padding: '10px' }}
+                            justifyContent="flex-start">
+                                <Typography noWrap>
+                                    Chat
+                                </Typography>
+                                <Typography noWrap>
+                                    Dashboard
+                                </Typography>
+                        </Stack>) : null
+                    }
+                    <Stack direction="row"
+                        justifyContent="flex-end">
+                        <Button component="label" variant="outlined">
+                            <input hidden accept="*" type="file" onChange={(e) => onUpload(e)} />
+                            <FileUpload />
+                            <Typography sx={{ textTransform: "none" }}>
+                                Upload
+                            </Typography>
+                        </Button>
+                        { darkModeSignal.value ?
+                            <IconButton aria-label="toggleLightmode"
+                                        onClick={() => darkModeSignal.value = false}
+                                        color='inherit'>
+                                <WbSunnyOutlined />
+                            </IconButton> :
+                            <IconButton aria-label="toggleDarkmode"
+                                        onClick={() => darkModeSignal.value = true}
+                                        color='info'>
+                                <Brightness2 />
+                            </IconButton>
+                        }
+                    </Stack>
                 </div>
             </Toolbar>
             <Sidebar/>
